99741e0319e4081fd007722e8f7fdf1b1cafb7ee
Graeme McAllister <graeme.mcallister07@gmail.com>
2020-08-13 00:23:20 +0100

    Parser with all options noted

--- parse.py
+++ parse.py
@@ -2,21 +2,81 @@ import requests
 import json
 from uk_covid19 import Cov19API
 
+
+
+
 scotland = [
-    'areaType=ltla',
-    'date=2020-08-08'
+    'areaName=Aberdeen City',
+    #'areaCode>=S00000000',
+    #'areaCode<=S99999999',
+    'date>=2020-08-11'
 ]
 
-cases_and_deaths = {
+customStructure = {
     "date": "date",
+    "areaType": "areaType",
     "areaName": "areaName",
     "areaCode": "areaCode",
-    "newCasesByPublishDate": "newCasesByPublishDate",
-    "cumCasesByPublishDate": "cumCasesByPublishDate",
-    "newDeathsByDeathDate": "newDeathsByDeathDate",
-    "cumDeathsByDeathDate": "cumDeathsByDeathDate"
+    "hash": "hash",
+    "cases": {
+        "daily":"newCasesByPublishDate",
+        "cumulative":"cumCasesByPublishDate"
+    },
+    "deaths": {
+        "daily":"newDeathsByDeathDate",
+        "cumulative":"cumDeathsByDeathDate"
+    },
+    "specimenDate": {
+    "newCasesBySpecimenDate": "newCasesBySpecimenDate",
+    "cumCasesBySpecimenDate": "cumCasesBySpecimenDate"    
+    }
 }
 
-api = Cov19API(filters=scotland, structure=cases_and_deaths)  # Returns a dictionary
-data = api.get_json(save_as="data.json")
+structureAll ={
+    "date": "date",
+    "areaType": "areaType",
+    "areaName": "areaName",
+    "areaCode": "areaCode",
+    "hash": "hash",
+    "cases": {
+        "daily":"newCasesByPublishDate",
+        "cumulative":"cumCasesByPublishDate"
+    },
+    "deaths": {
+        "daily":"newDeathsByDeathDate",
+        "cumulative":"cumDeathsByDeathDate"
+    },
+    "specimenDate": {
+    "newCasesBySpecimenDate": "newCasesBySpecimenDate",
+    "cumCasesBySpecimenDate": "cumCasesBySpecimenDate"    
+    },
+    "maleCases": "maleCases",
+    "femaleCases": "femaleCases",
+    "newPillarOneTestsByPublishDate": "newPillarOneTestsByPublishDate",
+    "cumPillarOneTestsByPublishDate": "cumPillarOneTestsByPublishDate",
+    "newPillarTwoTestsByPublishDate": "newPillarTwoTestsByPublishDate",
+    "cumPillarTwoTestsByPublishDate": "cumPillarTwoTestsByPublishDate",
+    "newPillarThreeTestsByPublishDate": "newPillarThreeTestsByPublishDate",
+    "cumPillarThreeTestsByPublishDate": "cumPillarThreeTestsByPublishDate",
+    "newPillarFourTestsByPublishDate": "newPillarFourTestsByPublishDate",
+    "cumPillarFourTestsByPublishDate": "cumPillarFourTestsByPublishDate",
+    "newAdmissions": "newAdmissions",
+    "cumAdmissions": "cumAdmissions",
+    "cumAdmissionsByAge": "cumAdmissionsByAge",
+    "cumTestsByPublishDate": "cumTestsByPublishDate",
+    "newTestsByPublishDate": "newTestsByPublishDate",
+    "covidOccupiedMVBeds": "covidOccupiedMVBeds",
+    "hospitalCases": "hospitalCases",
+    "plannedCapacityByPublishDate": "plannedCapacityByPublishDate",
+    "newDeathsByPublishDate": "newDeathsByPublishDate",
+    "cumDeathsByPublishDate": "cumDeathsByPublishDate",
+    "newDeathsByDeathDate": "newDeathsByDeathDate",
+    "cumDeathsByDeathDate": "cumDeathsByDeathDate",
+    "femaleDeaths": "femaleDeaths",
+    "maleDeaths": "maleDeaths"
+}
+
+api = Cov19API(filters=scotland, structure=customStructure)  # Returns a dictionary
+data = api.get_json("scotland.json")
 print(data)
+
